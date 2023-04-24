57b00498a6cb5a19677efb40621619dce6620604
Rodrigo Muñoz <maraushuaia@gmail.com>
2023-04-24 17:01:48 -0300

    Paso por PROPS el color del componente

--- src/components/Notification/NotificationView.jsx
+++ src/components/Notification/NotificationView.jsx
@@ -6,9 +6,9 @@ import React from 'react';
 import {Badge, IconButton} from '@mui/material';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
-const NotificationView = () => {
+const NotificationView = ({color}) => {
   return (
-    <IconButton size='large' aria-label='notification' color='inherit'>
+    <IconButton size='large' aria-label='notification' color={color}>
       <Badge badgeContent={1} color='info'>
         <NotificationsIcon />
       </Badge>
