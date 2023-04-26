856c828abe912ba550348ce0af9179a843eed7da
Rodrigo Muñoz <maraushuaia@gmail.com>
2023-04-26 16:11:38 -0300

    Incorporo codigo para simular muchas card y adecuo texto y estilos

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,22 +3,70 @@ import React from 'react';
 import CardRender from '../Card/CardRender';
 import ItemDetailContainer from '../ItemDetailContainer/ItemDetailContainer';
 
+
 const ItemListContainer = () => {
   const img ='https://res.cloudinary.com/dtvrg3by2/image/upload/v1678667310/CoderHoseReact/habilitaciones_qgpbs6.png'
   const alt = 'Alt de la imagen'
   const price = 12
-  const extract = 'Este es el extracto del producto'
+  const extract = 'Este es el extracto del producto para ver cuanto ocupa en espacio de letras.'
   
   return (
-    <Box>
+    <Box sx={{
+      // backgroundColor: '#003590',
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent:'center',
+
+      }}>
      <CardRender 
      img={img}
      alt={alt}
      price={price}
      extract={extract}
      />
-
-     <ItemDetailContainer/>
+     {/* <ItemDetailContainer/> */}
+     <CardRender 
+     img={img}
+     alt={alt}
+     price={price}
+     extract={extract}
+     />
+     <CardRender 
+     img={img}
+     alt={alt}
+     price={price}
+     extract={extract}
+     />
+     <CardRender 
+     img={img}
+     alt={alt}
+     price={price}
+     extract={extract}
+     />
+     <CardRender 
+     img={img}
+     alt={alt}
+     price={price}
+     extract={extract}
+     />
+     <CardRender 
+     img={img}
+     alt={alt}
+     price={price}
+     extract={extract}
+     />
+     <CardRender 
+     img={img}
+     alt={alt}
+     price={price}
+     extract={extract}
+     />
+     <CardRender 
+     img={img}
+     alt={alt}
+     price={price}
+     extract={extract}
+     />
     </Box>
   );
 };
