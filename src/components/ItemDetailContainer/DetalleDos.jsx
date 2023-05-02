ae26a5d81480d1bbef381cc8e45ea6728f7c7569
Rodrigo Muñoz <maraushuaia@gmail.com>
2023-05-02 16:39:55 -0300

    Adecuo puntos de quiebre para vista movil

--- src/components/ItemDetailContainer/DetalleDos.jsx
+++ src/components/ItemDetailContainer/DetalleDos.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
-import {Box, Grid, Typography} from '@mui/material';
+import React, { useState } from "react";
+import { Box, Grid, Typography } from "@mui/material";
 
-const DetalleDos = ({name, descriptionGeneral, price, images}) => {
+const DetalleDos = ({ name, descriptionGeneral, price, images }) => {
   const [selectedImage, setSelectedImage] = useState(images[0]);
 
   const handleImageSelect = (image) => {
@@ -9,46 +9,49 @@ const DetalleDos = ({name, descriptionGeneral, price, images}) => {
   };
 
   return (
-    <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <Grid
         sx={{
-          display: {md: 'flex'},
+          display: { md: "flex" },
           mb: 2,
         }}
         item
         xs={10}
-        flexDirection={{xs: 'row', sm: 'colum', md: 'colum', lg: 'colum'}}
+        flexDirection={{ xs: "row", sm: "colum", md: "colum", lg: "colum" }}
       >
         <Box
-          width={{xs: '400px', md: '800px', lg: '800px'}}
-          height={{xs: '420px', md: '620px', lg: '620px'}}
+          width={{ xs: "290px", sm: "370px", md: "800px", lg: "800px" }}
+          height={{ xs: "340px", sm: "400px", md: "620px", lg: "620px" }}
           sx={{
             backgroundImage: `url(${selectedImage.url})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'contain',
-            borderRadius: '4px',
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            borderRadius: "4px",
           }}
         />
         <Grid item xs={2}>
           <Box
             sx={{
-              display: {xs: 'inline-flex', md: 'flex', lg: 'flex'},
-              flexDirection: {xs: 'row', md: 'column', lg: 'colum'},
-              alignItems: 'flex-start',
+              display: { xs: "inline-flex", md: "flex", lg: "flex" },
+              flexDirection: { xs: "row", md: "column", lg: "colum" },
+              alignItems: "flex-start",
             }}
           >
             {images.map((image, index) => (
               <Box
                 key={index}
-                width={{xs: '80px', md: '100px', lg: '100px'}}
-                height={{xs: '80px', md: '100px', lg: '100px'}}
+                width={{ xs: "55px", sm: "75px", md: "100px", lg: "160px" }}
+                height={{ xs: "55px", sm: "75px", md: "100px", lg: "100px" }}
                 sx={{
                   backgroundImage: `url(${image.url})`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'cover',
-                  cursor: 'pointer',
-                  border: selectedImage === image ? '2px solid black' : 'none',
-                  borderRadius: '4px',
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "contain",
+                  cursor: "pointer",
+                  border: selectedImage === image ? "2px solid black" : "none",
+                  borderRadius: "4px",
+                  alignItems: "center",
                   m: 1,
                 }}
                 onClick={() => handleImageSelect(image)}
