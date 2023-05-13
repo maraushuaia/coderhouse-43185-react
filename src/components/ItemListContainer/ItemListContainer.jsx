4edff8264b5b6452765a214d6dad31b3270219a9
Rodrigo Muñoz <rodrigo.munoz@codein.coop.ar>
2023-05-13 16:53:54 -0300

    Cambio a JSON

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -22,10 +22,10 @@ const ItemListContainer = () => {
   const {id} = useParams();
 
   useEffect(() => {
-    // fetch(process.env.REACT_APP_URL_LOCAL)
-    fetch(process.env.REACT_APP_DIRECTUS_API_URL)
-      // .then((response) => response.json())
-      .then((data) => setProducts(data.data))
+    fetch(process.env.REACT_APP_URL_LOCAL)
+      // fetch(process.env.REACT_APP_DIRECTUS_API_URL)
+      .then((response) => response.json())
+      // .then((data) => setProducts(data.data))
       .then((data) => setProducts(data))
       .catch((error) => console.log(error));
   }, []);
@@ -67,105 +67,106 @@ const ItemListContainer = () => {
           gap={{xs: 1.5, sm: 2, md: 3, lg: 4}}
         >
           {products.map(
-            (product) =>
+            (product) => (
               // product.status === 'published' ? (
-              product.status === 'published' ? (
-                <CardActionArea
-                  key={product.id}
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    boxShadow: 5,
-                  }}
-                  onClick={() => handleClick(product.id)}
-                >
-                  <Card sx={{flexGrow: 1}}>
-                    <CardMedia
-                      component='img'
-                      image={product.imageMain}
-                      alt={product.name}
+
+              <CardActionArea
+                key={product.id}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  boxShadow: 5,
+                }}
+                onClick={() => handleClick(product.id)}
+              >
+                <Card sx={{flexGrow: 1}}>
+                  <CardMedia
+                    component='img'
+                    image={product.imageMain}
+                    alt={product.name}
+                    sx={{
+                      objectFit: 'contain',
+                      height: {
+                        xs: '90px',
+                        sm: '110px',
+                        md: '140px',
+                        lg: '140px',
+                      },
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  />
+
+                  <CardContent sx={{flexGrow: 1}}>
+                    <Box
                       sx={{
-                        objectFit: 'contain',
                         height: {
-                          xs: '90px',
-                          sm: '110px',
-                          md: '140px',
-                          lg: '140px',
+                          xs: '35px',
+                          sm: '40px',
+                          md: '50px',
+                          lg: '60px',
                         },
                         display: 'flex',
                         justifyContent: 'center',
                         alignItems: 'center',
                       }}
-                    />
+                    >
+                      <Typography
+                        gutterBottom
+                        variant='h5'
+                        component='h2'
+                        sx={{
+                          fontSize: {
+                            xs: '1.4em',
+                            sm: '1.5em',
+                            md: '1.7em',
+                            lg: '1.7em',
+                          },
+                        }}
+                      >
+                        {product.name}
+                      </Typography>
+                    </Box>
 
-                    <CardContent sx={{flexGrow: 1}}>
-                      <Box
-                        sx={{
-                          height: {
-                            xs: '35px',
-                            sm: '40px',
-                            md: '50px',
-                            lg: '60px',
-                          },
-                          display: 'flex',
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                        }}
-                      >
-                        <Typography
-                          gutterBottom
-                          variant='h5'
-                          component='h2'
-                          sx={{
-                            fontSize: {
-                              xs: '1.4em',
-                              sm: '1.5em',
-                              md: '1.7em',
-                              lg: '1.7em',
-                            },
-                          }}
-                        >
-                          {product.name}
-                        </Typography>
-                      </Box>
+                    <Box
+                      sx={{
+                        height: {
+                          xs: '90px',
+                          sm: '80px',
+                          md: '70px',
+                          lg: '80px',
+                        },
+                      }}
+                    >
+                      <Typography variant='body2' color='text.secondary'>
+                        {product.descriptionshort}
+                      </Typography>
+                    </Box>
+                    <Divider />
+                    <Box
+                      sx={{
+                        height: {
+                          xs: '30px',
+                          sm: '50px',
+                          md: '50px',
+                          lg: '80px',
+                        },
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        pt: {xs: 3.5, sm: 1, md: 1, lg: 1},
+                      }}
+                    >
+                      <Typography gutterBottom variant='h6' component='div'>
+                        ${product.price}
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </CardActionArea>
+            )
 
-                      <Box
-                        sx={{
-                          height: {
-                            xs: '90px',
-                            sm: '80px',
-                            md: '70px',
-                            lg: '80px',
-                          },
-                        }}
-                      >
-                        <Typography variant='body2' color='text.secondary'>
-                          {product.descriptionshort}
-                        </Typography>
-                      </Box>
-                      <Divider />
-                      <Box
-                        sx={{
-                          height: {
-                            xs: '30px',
-                            sm: '50px',
-                            md: '50px',
-                            lg: '80px',
-                          },
-                          display: 'flex',
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                          pt: {xs: 3.5, sm: 1, md: 1, lg: 1},
-                        }}
-                      >
-                        <Typography gutterBottom variant='h6' component='div'>
-                          ${product.price}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </CardActionArea>
-              ) : null
             // ) : null
           )}
         </Box>
