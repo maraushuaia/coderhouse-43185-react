447a93746535a6daa505606f69d82aed1daee5af
Rodrigo Muñoz <maraushuaia@gmail.com>
2023-05-16 10:39:37 -0300

    Incorporo uso de variables de entorno Directus

--- src/components/hooks/useSearchProducts.jsx
+++ src/components/hooks/useSearchProducts.jsx
@@ -1,22 +1,25 @@
 // Este componente solo efectúa la busqueda de productos en la API que le indico mediante variables de entorno
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
 const useSearchProducts = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log('datos del fetch => ', process.env.REACT_APP_DIRECTUS_API_URL);
+    console.log(
+      "Variable Entorno para el Fetch => ",
+      process.env.REACT_APP_DIRECTUS_API_URL
+    );
     fetch(process.env.REACT_APP_DIRECTUS_API_URL)
       .then((response) => response.json())
       .then((data) => {
-        setProducts(data);
+        setProducts(data.data);
         setLoading(false);
       })
       .catch((error) => console.log(error));
   }, []);
-
-  return {products, loading};
+  console.log("Productos Encontrados por Busqueda => ", products);
+  return { products, loading };
 };
 
 export default useSearchProducts;
