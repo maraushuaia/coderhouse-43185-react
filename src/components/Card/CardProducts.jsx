f7c8ad0f7c21e7eaea04ce93edd333f2aa0c98e5
Rodrigo Muñoz <rodrigo.munoz@codein.coop.ar>
2023-06-08 21:12:36 -0300

    Cambios en card

--- src/components/Card/CardProducts.jsx
+++ src/components/Card/CardProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, {useState, useContext} from 'react';
 import {
   Box,
   Card,
@@ -8,48 +8,48 @@ import {
   Divider,
   IconButton,
   Typography,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { ContextFavorites } from "../../contexts/ContextFavorites";
+} from '@mui/material';
+import {Link} from 'react-router-dom';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import {ContextFavorites} from '../../contexts/ContextFavorites';
 
-const CardProducts = ({ product }) => {
+const CardProducts = ({product}) => {
   const imageHeight = {
-    xs: "90px",
-    sm: "110px",
-    md: "140px",
-    lg: "140px",
+    xs: '90px',
+    sm: '110px',
+    md: '140px',
+    lg: '140px',
   };
 
   const titleHeight = {
-    xs: "35px",
-    sm: "40px",
-    md: "50px",
-    lg: "60px",
+    xs: '35px',
+    sm: '40px',
+    md: '50px',
+    lg: '60px',
   };
 
   const descriptionHeight = {
-    xs: "90px",
-    sm: "80px",
-    md: "70px",
-    lg: "80px",
+    xs: '90px',
+    sm: '80px',
+    md: '70px',
+    lg: '80px',
   };
 
   const priceHeight = {
-    xs: "30px",
-    sm: "50px",
-    md: "50px",
-    lg: "80px",
+    xs: '30px',
+    sm: '50px',
+    md: '50px',
+    lg: '80px',
   };
 
   const titleFontSize = {
-    xs: "1.4em",
-    sm: "1.5em",
-    md: "1.7em",
-    lg: "1.7em",
+    xs: '1.4em',
+    sm: '1.5em',
+    md: '1.7em',
+    lg: '1.7em',
   };
 
-  const { addFavorite, removeFavorite, isFavorite } =
+  const {addFavorite, removeFavorite, isFavorite} =
     useContext(ContextFavorites);
 
   const [isProductFavorite, setIsProductFavorite] = useState(
@@ -63,78 +63,86 @@ const CardProducts = ({ product }) => {
       addFavorite(product);
     }
     setIsProductFavorite(!isProductFavorite);
-    console.log("isProductFavorite", isProductFavorite);
+    console.log('isProductFavorite', isProductFavorite);
   };
 
   return (
     <Box
       key={product.id}
       sx={{
-        display: "flex",
-        flexDirection: "column",
+        display: 'flex',
+        flexDirection: 'column',
         boxShadow: 5,
-        maxWidth: "220px",
+        maxWidth: '220px',
       }}
     >
-      <Card sx={{ flexGrow: 1 }}>
-        <Link
-          to={`/itemDetail/${product.id}`}
-          style={{ textDecoration: "none" }}
-        >
-          <CardActionArea sx={{ maxWidth: "220px" }}>
+      <Card sx={{flexGrow: 1}}>
+        <Link to={`/itemDetail/${product.id}`} style={{textDecoration: 'none'}}>
+          <CardActionArea sx={{maxWidth: '220px'}}>
             <CardMedia
-              component="img"
+              component='img'
               image={product.imageMain}
               alt={product.name}
               sx={{
-                objectFit: "contain",
+                objectFit: 'contain',
                 height: imageHeight,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                position: "relative",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                position: 'relative',
               }}
             />
 
-            <CardContent sx={{ flexGrow: 1 }}>
+            <CardContent sx={{flexGrow: 1}}>
               <Box
                 sx={{
                   height: titleHeight,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
                 }}
+                mb={2}
               >
                 <Typography
                   gutterBottom
-                  variant="h5"
+                  variant='span'
                   sx={{
-                    fontSize: titleFontSize,
+                    // fontSize: titleFontSize,
+                    fontSize: '1.2em',
                   }}
                 >
                   {product.name}
                 </Typography>
               </Box>
 
-              <Box sx={{ height: descriptionHeight }}>
-                <Typography variant="body2" color="text.secondary">
+              <Box sx={{height: descriptionHeight}}>
+                <Typography variant='body2' color='text.secondary'>
+                  {product.descriptionShort.length > 70
+                    ? product.descriptionShort.substring(0, 70) + '...'
+                    : product.descriptionShort}
+                </Typography>
+              </Box>
+
+              {/* <Box sx={{height: descriptionHeight}}>
+                <Typography variant='body2' color='text.secondary'>
                   {product.descriptionshort}
                 </Typography>
               </Box>
+            </CardContent> */}
             </CardContent>
           </CardActionArea>
         </Link>
         <Divider />
         <Box
-          display={"flex"}
-          justifyContent={"center"}
-          mt={{ xs: 1, sm: 1, md: 1, lg: 1 }}
+          display={'flex'}
+          justifyContent={'center'}
+          mt={{xs: 1, sm: 1, md: 1, lg: 1}}
           pr={1}
         >
           <IconButton
-            aria-label="add to favorites"
+            aria-label='add to favorites'
             onClick={handleFavoriteClick}
-            color={isProductFavorite ? "error" : "inherit"}
+            color={isProductFavorite ? 'error' : 'inherit'}
           >
             <FavoriteIcon />
           </IconButton>
@@ -142,14 +150,14 @@ const CardProducts = ({ product }) => {
         <Box
           sx={{
             height: priceHeight,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            pt: { xs: 1, sm: 1, md: 1, lg: 1 },
-            pb: { xs: 1, sm: 1, md: 1, lg: 1 },
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            // pt: {xs: 1, sm: 1, md: 1, lg: 1},
+            // pb: {xs: 1, sm: 1, md: 1, lg: 1},
           }}
         >
-          <Typography gutterBottom variant="h6" component="div">
+          <Typography gutterBottom variant='h6' component='div'>
             ${product.price}
           </Typography>
         </Box>
