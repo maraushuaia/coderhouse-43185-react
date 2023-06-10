c2d727952551225191136900b89aecbcc315c2a0
Rodrigo Muñoz <rodrigo.munoz@codein.coop.ar>
2023-06-10 15:19:41 -0300

    Limpio codigo que no usaré

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,41 +1,5 @@
-// import {Container, Typography} from '@mui/material';
-// import Carrusel from '../components/Carrusel/Carrusel';
-// import ItemListContainer from '../components/ItemListContainer/ItemListContainer';
-
-// const HomePage = () => {
-//   return (
-//     <Container maxWidth='xl' sx={{mt: 4, minHeight: '80vh'}}>
-//       <section>
-//         <Typography variant='h5' component='h5'>
-//           Ofertas
-//         </Typography>
-//         <Carrusel category={'Lenceria'} />
-//       </section>
-
-//       <section>
-//         <Typography variant='h5' component='h5'>
-//           Lencería
-//         </Typography>
-//         <Carrusel />
-//       </section>
-
-//       <section>
-//         <Typography variant='h5' component='h5'>
-//           Favoritos
-//         </Typography>
-//         <Carrusel />
-//       </section>
-
-//       {/* <ItemListContainer /> */}
-//     </Container>
-//   );
-// };
-
-// export default HomePage;
-
 import {Container, Typography} from '@mui/material';
 import Carrusel from '../components/Carrusel/Carrusel';
-import ItemListContainer from '../components/ItemListContainer/ItemListContainer';
 
 const HomePage = () => {
   return (
